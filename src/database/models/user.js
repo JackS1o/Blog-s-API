ad3ff09035f12a10b95b346153fe365cb662a99a
Jackson Viana <jviana116@gmail.com>
2022-08-25 17:03:12 -0300

    feat: adicionando a model user

---
+++ src/database/models/user.js
@@ -0,0 +1,28 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    displayName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  });
+  return User;
+}
