fabb15abcb197860acabce50e19d9d5233b3a0f6
Jackson Viana <jviana116@gmail.com>
2022-08-25 21:41:20 -0300

    feat: criando model category

---
+++ src/database/models/category.js
@@ -0,0 +1,18 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  },
+  {
+    timestamps: false,
+  });
+  return Category;
+};
